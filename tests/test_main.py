0cd5429e141e47dfa109e31512ab87e2a9d10566
Marc Partensky <marc.partensky@gmail.com>
2025-02-28 17:57:58 +0100

    tests

---
+++ tests/test_main.py
@@ -0,0 +1,32 @@
+from fastapi.testclient import TestClient
+
+import sys
+import os
+
+# Ajouter le dossier parent au sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from app.main import app
+from fastapi.websockets import WebSocket
+
+client = TestClient(app)
+
+
+def test_websocket():
+    with client.websocket_connect("/ws/test-client") as websocket:
+        websocket.send_text("Hello WebSocket")
+        data = websocket.receive_text()
+        assert "Client #test-client dit: Hello WebSocket" in data
+
+
+def test_echo_message():
+    test_message = {"text": "Hello FastAPI!"}
+    response = client.post("/echo", json=test_message)
+    assert response.status_code == 200
+    assert response.json() == {"message": test_message["text"]}
+
+
+def test_websocket_echo():
+    with client.websocket_connect("/ws/test-client") as websocket:
+        websocket.send_text("Hello WebSocket")
+        response = websocket.receive_text()
+        assert response == "Client #test-client dit: Hello WebSocket"
